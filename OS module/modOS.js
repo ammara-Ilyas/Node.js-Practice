c00198b5a1b7f0adab040ed2fca1adb43a6207d7
ammara-Ilyas <ismarailyas545@gmail.com>
2024-07-11 16:46:54 +0500

    OS in node

---
+++ OS module/modOS.js
@@ -0,0 +1,8 @@
+const os = require("os");
+console.log(os.arch());
+
+console.log(os.EOL);
+// console.log(os.cpus());
+console.log(os.hostname());
+console.log(os.machine());
+console.log(os.networkInterfaces());
