65fb56ee0b8d77d835a226ac687e1028ad3a4ba0
ammara-Ilyas <ismarailyas545@gmail.com>
2024-07-11 17:04:44 +0500

    Path in node

---
+++ path/path.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+console.log(path.dirname("C:UsersAMCDesktopNode/file system/file.js"));
+console.log(path.extname("C:UsersAMCDesktopNode/file system/file.js"));
+console.log(
+  "basename",
+  path.basename("C:UsersAMCDesktopNode/file system/file.js")
+);
+console.log("parse", path.parse("C:UsersAMCDesktopNode/file system/file.js"));
+// console.log(path.win32);
